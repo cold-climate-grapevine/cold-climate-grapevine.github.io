4725a3a0bfb6df49d1952a4e50cb6ba7159dfaf3
cold-climate-grapevine <drunkelevator@gmail.com>
2022-09-20 23:59:02 +0500

    feat: update SingleImageZoom

--- src/components/SingleImageZoom/index.js
+++ src/components/SingleImageZoom/index.js
@@ -1,35 +1,41 @@
+// How to combine IdealImage and ImageZoom?
+// IdealImage works during page scrolling, and ImageZoom works after building
+// Workaround: handle each image individually using Waypoint
+
 import React, { Component } from "react";
 import mediumZoom from "medium-zoom";
-import { Waypoint } from "react-waypoint";
 import Image from "@theme/IdealImage";
 
 class SingleImageZoom extends Component {
   state = {
-    tag: false,
     random: 0,
   };
-
-  _handleWaypointEnter = () => {
-    this.setState({ tag: true });
+  _handleImageLoaded = () => {
     const rand = Math.floor(Math.random() * 200);
     this.setState({ random: this.state.random + rand }, function () {
       setTimeout(() => {
+        // About medium-zoom (but this in building time): https://github.com/francoischalifour/medium-zoom
+        // But idea (after building) I took from here: https://github.com/flexanalytics/plugin-image-zoom/blob/master/src/zoom.js
         mediumZoom("#single-image-zoom-" + this.state.random, {
           background: "#000",
         });
       }, 1000);
     });
   };
+
   render() {
     return (
-      <Waypoint onEnter={this._handleWaypointEnter}>
-        {this.state.tag ? (
-          <Image
-            id={"single-image-zoom-" + this.state.random}
-            img={this.props.img}
-          />
-        ) : null}
-      </Waypoint>
+      // Read more about Waypoint: https://github.com/civiccc/react-waypoint
+      // First, read how to work IdealImage: https://github.com/stereobooster/react-ideal-image/blob/master/introduction.md
+      <Image
+        loaded={this._handleImageLoaded.bind(this)}
+        id={"single-image-zoom-" + this.state.random}
+        img={this.props.img}
+        alt={this.props.img.src.src
+          .replace(/(?:\/([a-z-]*))+.*/, "$1")
+          .replace(/-/g, " ")
+          .replace(/^./, (str) => str.toUpperCase())}
+      />
     );
   }
 }
